ce025bdb48c0b64cbb7fd4b3f3ac1a8a79586682
Srijayanth Sridhar <srijayanth@gmail.com>
2016-11-09 11:25:44 +0530

    Basic detection of a blocked path

--- createMaze.js
+++ createMaze.js
@@ -6,6 +6,10 @@ var randomDirection=function() {
   return ["north","south","east","west"][randomNumber(4)];
 }
 
+var sumOfCoordinates=function(c1,c2) {
+  return [c1[0]+c2[0], c1[1]+c2[1]];
+}
+
 var createGrid=function(rows,cols,character) {
   var grid=[];
   for (var i = 0; i < rows; i++) {
@@ -29,8 +33,24 @@ var directions={
   west: {val: west, left: north, right: south}
 };
 
-var outOfBounds=function(x,y,mx,my) {
+var outOfBounds=function(coord,mx,my) {
+  var x=coord[0];
+  var y=coord[1];
   return (x<0 || y<0 || x>=mx || y>=my);
+}
+
+var isCoordMazeChar=function(grid,coord) {
+  return grid[coord[0]][coord[1]]==MAZE_CHAR;
+}
+
+var pathBlocked=function(grid,coord,direction) {
+  var left=sumOfCoordinates(coord,directions[direction].left);
+  var right=sumOfCoordinates(coord,directions[direction].right);
+  var inFront=sumOfCoordinates(coord,directions[direction].val);
+
+  return (!outOfBounds(left,MAX_ROWS,MAX_COLS) && isCoordMazeChar(grid,left))
+    || (!outOfBounds(right,MAX_ROWS,MAX_COLS) && isCoordMazeChar(grid,right))
+    || (!outOfBounds(inFront,MAX_ROWS,MAX_COLS) && isCoordMazeChar(grid,inFront));
 }
 
 var generateMaze=function(grid,coord,direction) {
@@ -40,11 +60,15 @@ var generateMaze=function(grid,coord,direction) {
   var dRow=dir[0];
   var dCol=dir[1];
 
-  if(outOfBounds(row,col,MAX_ROWS,MAX_COLS) || )
+  if(outOfBounds(coord,MAX_ROWS,MAX_COLS))
     return grid;
-
-  grid[row][col]="X";
-  return generateMaze(grid,[row+dRow,col+dCol],direction)
+  if(grid[row][col]==MAZE_CHAR)
+    return grid;
+  if(pathBlocked(grid,coord,direction))
+    return grid;
+  grid[row][col]=MAZE_CHAR;
+  grid=generateMaze(grid,[row+dRow,col+dCol],direction);
+  return grid;
 }
 
 var MAX_ROWS=5;
@@ -53,10 +77,11 @@ var MAIN_CHAR="-";
 var MAZE_CHAR="X";
 
 var mainGrid=createGrid(MAX_ROWS,MAX_COLS,MAIN_CHAR);
+mainGrid[0][4]=MAZE_CHAR;
 
 var initCoord=[randomNumber(MAX_ROWS),randomNumber(MAX_COLS)];
 var initDirection=randomDirection();
-var maze=generateMaze(mainGrid,initCoord,initDirection);
-console.log(initDirection);
-console.log(initCoord);
+var maze=generateMaze(mainGrid,[0,0],"east");
+// console.log(initDirection);
+// console.log(initCoord);
 console.log(maze);
