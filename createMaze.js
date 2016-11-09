b85be36f967dcd5d3ca052d43013664cadbd92c5
Srijayanth Sridhar <srijayanth@gmail.com>
2016-11-09 15:57:41 +0530

    A broken first cut solution

--- createMaze.js
+++ createMaze.js
@@ -100,22 +100,26 @@ var createMazePath=function(grid,coord,direction) {
 
 var createMaze=function(grid,initCoord,initDir) {
   var grid=createMazePath(grid,initCoord,initDir);
-  var startingRows=grid.filter(function(r,i){
-    return r[1]==MAZE_CHAR;
-  });
-  var endingRows=grid.filter(function(r,i){
-    return r[r.length-2]==MAZE_CHAR;
-  });
-  startingRows[randomNumber(startingRows.length)][0]=MAZE_CHAR;
-  var randomEndingRow=endingRows[randomNumber(endingRows.length)];
+  var startingRows=[];
+  var endingRows=[];
+  for (var i = 0; i < grid.length; i++) {
+    if(grid[i][1]==MAZE_CHAR)
+      startingRows.push(i);
+    if(grid[i][MAX_COLS-2]==MAZE_CHAR)
+      endingRows.push(i);
+  }
+  var startRow=randomNumber(startingRows.length);
+  grid[startingRows[startRow]][0]=MAZE_CHAR;
+  var endRow=randomNumber(endingRows.length)
+  var randomEndingRow=grid[endRow];
   randomEndingRow.pop();
   randomEndingRow.push(MAZE_CHAR);
-  return grid;
+  return {grid: grid, start: [startingRows[startRow],0], end: [endingRows[endRow],MAX_COLS-1]};
 }
 
-var MAX_ROWS=50;
-var MAX_COLS=50;
-var MAIN_CHAR=" ";
+var MAX_ROWS=8;
+var MAX_COLS=8;
+var MAIN_CHAR="-";
 var MAZE_CHAR="X";
 
 var mainGrid=createGrid(MAX_ROWS,MAX_COLS,MAIN_CHAR);
@@ -124,19 +128,55 @@ var initCoord=[randomNumber(MAX_ROWS-2)+1,randomNumber(MAX_COLS-2)+1];
 var initDirection=randomDirection();
 var mainMaze=createMaze(mainGrid,initCoord,initDirection);
 
-var drawChart=function(maze) {
-  var rows=d3.select(".maze")
-    .selectAll("tr")
-    .data(maze)
-    .enter()
-    .append("tr");
-  rows.selectAll("td")
-    .data(function(d){return d})
-    .enter()
-    .append("td")
-    .attr("class",function(d,i){
-      return d==MAZE_CHAR?"path":"empty";
-    });
+// var drawChart=function(maze) {
+//   var rows=d3.select(".maze")
+//     .selectAll("tr")
+//     .data(maze.grid)
+//     .enter()
+//     .append("tr");
+//   rows.selectAll("td")
+//     .data(function(d){return d})
+//     .enter()
+//     .append("td")
+//     .attr("class",function(d,i){
+//       return d==MAZE_CHAR?"path":"empty";
+//     });
+// }
+
+var isSameCoord=function(c1,c2) {
+  return (c1[0]==c2[0])&&(c1[1]==c2[1]);
+}
+var solveMaze=function(grid,start,stop,solution) {
+  for (var i = 0; i < solution.length; i++) {
+    if(isSameCoord(solution[i],start))
+      return undefined;
+  }
+  if(isSameCoord(start,stop))
+    return solution;
+  var r=start[0];
+  var c=start[1];
+
+  if(r<0 || c<0 || r>=MAX_ROWS || c>=MAX_COLS)
+    return undefined;
+  if(grid[r][c]!= MAZE_CHAR)
+    return undefined;
+  solution.push(start);
+  if(solveMaze(grid,[r+1,c],stop,solution))
+    return solution;
+  if(solveMaze(grid,[r,c+1],stop,solution))
+    return solution;
+  if(solveMaze(grid,[r-1,c],stop,solution))
+    return solution;
+  if(solveMaze(grid,[r,c-1],stop,solution))
+    return solution;
+  solution.pop();
+  return undefined;
 }
 
-drawChart(mainMaze);
+var solve=function(maze) {
+  return solveMaze(maze.grid,maze.start,maze.end,[]);
+}
+
+// drawChart(mainMaze);
+console.log(mainMaze.grid);
+console.log(solve(mainMaze));
